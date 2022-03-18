07a9193144ad774dd9f3de5ba5b76e1773d57f49
RobbyPratl <robbypratl@outlook.com>
2022-03-17 23:15:50 -0500

    add move checking

--- chess/src/App.js
+++ chess/src/App.js
@@ -13,16 +13,49 @@ class App extends React.Component {
 // Cant use setState inside conditional so we must take in value, set to variable, 
 // change variable then pass out new state
   currentClick(notation) {
-    let x = this.state.board
-    this.setState({current_click: notation})
+    this.setState(
+    {
+      current_click: notation,
+      moves: []
+    
+    })
     
   }
+/* 
+Explanations:
+  * () => moves(x) checks last clicked sqaures, assuming we pass in values
+      in order to know in the future when to switch turns and stop players 
+      from not making a move.
 
+TODO: 
+
+  * -- COMPLETE -- add tuple to allow for moves
+
+  * Add state variable to change turns and add logic to change turns after a legal move
+
+  * add onClick={() => this.moves(`VALUE`)} to every board cell div
+
+  * map innerHTML of board cell divs to 
+
+  * add this.state.board
+
+  * allow this.board to move if this.local_moves reads a legal move &
+    not another of own colors piece located in said cell
+
+  * add taking pieces while storing taken pieces in another variable
+    (will be used later to calculate point values)
+
+  * add move legality
+
+  * add en passent 
+
+
+*/
   moves(x){
     this.local_moves = this.state.moves
-    if(this.local_moves.length == 2 /* OR this.local_moves[0] == x */){
+    if(this.local_moves.length === 2 || this.local_moves[0] === x/* OR this.local_moves[0] == x */){
       this.local_moves = []
-    }else if(this.local_moves.length == 1){
+    }else if(this.local_moves.length === 1){
       this.local_moves[1] = x;
     }else{
       this.local_moves[0] = x;
@@ -39,21 +72,21 @@ class App extends React.Component {
         <div className="chessboard">
           
           <div onClick={() => this.moves('a8')} className="white">{"♜"}</div>
-          <div onClick={() => this.currentClick('b8')} className="black">♞</div>
-          <div onClick={() => this.currentClick('c8')} className="white">♝</div>
-          <div onClick={() => this.currentClick('d8')} className="black">♛</div>
-          <div onClick={() => this.currentClick('e8')} className="white">♚</div>
-          <div onClick={() => this.currentClick('f8')} className="black">♝</div>
-          <div onClick={() => this.currentClick('g8')} className="white">♞</div>
-          <div onClick={() => this.currentClick('h8')} className="black">♜</div>
-          <div onClick={() => this.currentClick('a7')} className="black">♝</div>
-          <div onClick={() => this.currentClick('b7')} className="white">♝</div>
-          <div onClick={() => this.currentClick('c7')} className="black">♝</div>
-          <div onClick={() => this.currentClick('d7')} className="white">♝</div>
-          <div onClick={() => this.currentClick('e7')} className="black">♝</div>
-          <div onClick={() => this.currentClick('f7')} className="white">♝</div>
-          <div onClick={() => this.currentClick('g7')} className="black">♝</div>
-          <div onClick={() => this.currentClick('h7')} className="white">♝</div>
+          <div onClick={() => this.moves('b8')} className="black">♞</div>
+          <div onClick={() => this.moves('c8')} className="white">♝</div>
+          <div onClick={() => this.moves('d8')} className="black">♛</div>
+          <div onClick={() => this.moves('e8')} className="white">♚</div>
+          <div onClick={() => this.moves('f8')} className="black">♝</div>
+          <div onClick={() => this.moves('g8')} className="white">♞</div>
+          <div onClick={() => this.moves('h8')} className="black">♜</div>
+          <div onClick={() => this.moves('a7')} className="black">♝</div>
+          <div onClick={() => this.moves('b7')} className="white">♝</div>
+          <div onClick={() => this.moves('c7')} className="black">♝</div>
+          <div onClick={() => this.moves('d7')} className="white">♝</div>
+          <div onClick={() => this.moves('e7')} className="black">♝</div>
+          <div onClick={() => this.moves('f7')} className="white">♝</div>
+          <div onClick={() => this.moves('g7')} className="black">♝</div>
+          <div onClick={() => this.moves('h7')} className="white">♝</div>
           <div className="white"></div>
           <div className="black"></div>
           <div className="white"></div>
