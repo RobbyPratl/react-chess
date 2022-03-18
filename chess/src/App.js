b59536ce5695c9a3ded63486712c10e8947409ee
RobbyPratl <robbypratl@outlook.com>
2022-03-17 21:49:30 -0500

    add logic

--- chess/src/App.js
+++ chess/src/App.js
@@ -1,76 +1,90 @@
 import './App.css';
+import React from 'react';
+
 // note onclick works with react !
-function App() {
-  return (
-      <html>
-<div className="chessboard">
-<div onClick={() => console.log("work?")} className="white">♜</div>
-<div className="black">♞</div>
-<div className="white">♝</div>
-<div className="black">♛</div>
-<div className="white">♚</div>
-<div className="black">♝</div>
-<div className="white">♞</div>
-<div className="black">♜</div>
-<div className="black">♝</div>
-<div className="white">♝</div>
-<div className="black">♝</div>
-<div className="white">♝</div>
-<div className="black">♝</div>
-<div className="white">♝</div>
-<div className="black">♝</div>
-<div className="white">♝</div>
-<div className="white"></div>
-<div className="black"></div>
-<div className="white"></div>
-<div className="black"></div>
-<div className="white"></div>
-<div className="black"></div>
-<div className="white"></div>
-<div className="black"></div>
-<div className="black"></div>
-<div className="white"></div>
-<div className="black"></div>
-<div className="white"></div>
-<div className="black"></div>
-<div className="white"></div>
-<div className="black"></div>
-<div className="white"></div>
-<div className="white"></div>
-<div className="black"></div>
-<div className="white"></div>
-<div className="black"></div>
-<div className="white"></div>
-<div className="black"></div>
-<div className="white"></div>
-<div className="black"></div>
-<div className="black"></div>
-<div className="white"></div>
-<div className="black"></div>
-<div className="white"></div>
-<div className="black"></div>
-<div className="white"></div>
-<div className="black"></div>
-<div className="white"></div>
-<div className="white">&#9817;</div>
-<div className="black">&#9817;</div>
-<div className="white">&#9817;</div>
-<div className="black">&#9817;</div>
-<div className="white">&#9817;</div>
-<div className="black">&#9817;</div>
-<div className="white">&#9817;</div>
-<div className="black">&#9817;</div>
-<div className="black">&#9814;</div>
-<div className="white">&#9816;</div>
-<div className="black">&#9815;</div>
-<div className="white">&#9813;</div>
-<div className="black">&#9812;</div>
-<div className="white">&#9815;</div>
-<div className="black">&#9816;</div>
-<div className="white">&#9814;</div>
-</div>
-      </html>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {current_click: ''};
+  }
+
+  currentClick(notation) {
+    this.setState({current_click: notation})
+  }
+
+  render (){
+    return (
+      <div>
+        <div className="chessboard">
+          <div onClick={() => this.currentClick('a8')} className="white">♜</div>
+          <div className="black">♞</div>
+          <div className="white">♝</div>
+          <div className="black">♛</div>
+          <div className="white">♚</div>
+          <div className="black">♝</div>
+          <div className="white">♞</div>
+          <div className="black">♜</div>
+          <div className="black">♝</div>
+          <div className="white">♝</div>
+          <div className="black">♝</div>
+          <div className="white">♝</div>
+          <div className="black">♝</div>
+          <div className="white">♝</div>
+          <div className="black">♝</div>
+          <div className="white">♝</div>
+          <div className="white"></div>
+          <div className="black"></div>
+          <div className="white"></div>
+          <div className="black"></div>
+          <div className="white"></div>
+          <div className="black"></div>
+          <div className="white"></div>
+          <div className="black"></div>
+          <div className="black"></div>
+          <div className="white"></div>
+          <div className="black"></div>
+          <div className="white"></div>
+          <div className="black"></div>
+          <div className="white"></div>
+          <div className="black"></div>
+          <div className="white"></div>
+          <div className="white"></div>
+          <div className="black"></div>
+          <div className="white"></div>
+          <div className="black"></div>
+          <div className="white"></div>
+          <div className="black"></div>
+          <div className="white"></div>
+          <div className="black"></div>
+          <div className="black"></div>
+          <div className="white"></div>
+          <div className="black"></div>
+          <div className="white"></div>
+          <div className="black"></div>
+          <div className="white"></div>
+          <div className="black"></div>
+          <div className="white"></div>
+          <div className="white">&#9817;</div>
+          <div className="black">&#9817;</div>
+          <div className="white">&#9817;</div>
+          <div className="black">&#9817;</div>
+          <div className="white">&#9817;</div>
+          <div className="black">&#9817;</div>
+          <div className="white">&#9817;</div>
+          <div className="black">&#9817;</div>
+          <div className="black">&#9814;</div>
+          <div className="white">&#9816;</div>
+          <div className="black">&#9815;</div>
+          <div className="white">&#9813;</div>
+          <div className="black">&#9812;</div>
+          <div className="white">&#9815;</div>
+          <div className="black">&#9816;</div>
+          <div className="white">&#9814;</div>
+        </div>
+      <h1>{this.state.current_click}</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
