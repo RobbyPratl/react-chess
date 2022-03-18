81cc93d43a51612145e5d72176f3e30a493e7cb8
RobbyPratl <robbypratl@outlook.com>
2022-03-17 21:38:57 -0500

    add board

--- chess/src/App.js
+++ chess/src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
-
+// note onclick works with react !
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <html>
+<div className="chessboard">
+<div onClick={() => console.log("work?")} className="white">♜</div>
+<div className="black">♞</div>
+<div className="white">♝</div>
+<div className="black">♛</div>
+<div className="white">♚</div>
+<div className="black">♝</div>
+<div className="white">♞</div>
+<div className="black">♜</div>
+<div className="black">♝</div>
+<div className="white">♝</div>
+<div className="black">♝</div>
+<div className="white">♝</div>
+<div className="black">♝</div>
+<div className="white">♝</div>
+<div className="black">♝</div>
+<div className="white">♝</div>
+<div className="white"></div>
+<div className="black"></div>
+<div className="white"></div>
+<div className="black"></div>
+<div className="white"></div>
+<div className="black"></div>
+<div className="white"></div>
+<div className="black"></div>
+<div className="black"></div>
+<div className="white"></div>
+<div className="black"></div>
+<div className="white"></div>
+<div className="black"></div>
+<div className="white"></div>
+<div className="black"></div>
+<div className="white"></div>
+<div className="white"></div>
+<div className="black"></div>
+<div className="white"></div>
+<div className="black"></div>
+<div className="white"></div>
+<div className="black"></div>
+<div className="white"></div>
+<div className="black"></div>
+<div className="black"></div>
+<div className="white"></div>
+<div className="black"></div>
+<div className="white"></div>
+<div className="black"></div>
+<div className="white"></div>
+<div className="black"></div>
+<div className="white"></div>
+<div className="white">&#9817;</div>
+<div className="black">&#9817;</div>
+<div className="white">&#9817;</div>
+<div className="black">&#9817;</div>
+<div className="white">&#9817;</div>
+<div className="black">&#9817;</div>
+<div className="white">&#9817;</div>
+<div className="black">&#9817;</div>
+<div className="black">&#9814;</div>
+<div className="white">&#9816;</div>
+<div className="black">&#9815;</div>
+<div className="white">&#9813;</div>
+<div className="black">&#9812;</div>
+<div className="white">&#9815;</div>
+<div className="black">&#9816;</div>
+<div className="white">&#9814;</div>
+</div>
+      </html>
   );
 }
 
